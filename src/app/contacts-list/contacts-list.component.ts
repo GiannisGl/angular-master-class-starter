2f966db7cfaf2495a552be288518014334a18fac
Ioannis Glampedakis <Ioannis.Glampedakis@netcetera.com>
2021-11-09 13:58:33 +0100

    observables: exercise 3

--- src/app/contacts-list/contacts-list.component.ts
+++ src/app/contacts-list/contacts-list.component.ts
@@ -1,25 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Contact} from '../models/contact';
 import {ContactsService} from '../contacts.service';
-import {Observable, Subject} from 'rxjs';
-import {debounceTime, distinctUntilChanged} from 'rxjs/operators';
+import {merge, Observable, Subject} from 'rxjs';
+import {debounceTime, delay, distinctUntilChanged, switchMap, takeUntil} from 'rxjs/operators';
 
 @Component({
   selector: 'trm-contacts-list',
   templateUrl: './contacts-list.component.html',
   styleUrls: ['./contacts-list.component.scss']
 })
-export class ContactsListComponent {
-  contacts$: Observable<Contact[]> = this.contactsService.getContacts();
+export class ContactsListComponent implements OnInit {
+  contacts$: Observable<Contact[]>;
   private terms$: Subject<string> = new Subject<string>();
 
   constructor(private contactsService: ContactsService) {
-    this.terms$
+  }
+
+  ngOnInit(): void {
+    const searchContacts$ = this.terms$
       .pipe(
         distinctUntilChanged(),
-        debounceTime(400)
-      )
-      .subscribe((term: string) => this.searchList(term));
+        debounceTime(400),
+        switchMap((term: string) => this.searchList(term))
+      );
+
+    const allContacts$ = this.contactsService.getContacts()
+      .pipe(
+        delay(5000),
+        takeUntil(searchContacts$)
+      );
+
+    this.contacts$ = merge(allContacts$, searchContacts$);
   }
 
   trackById(index: number, contact: Contact): string | number {
@@ -31,6 +42,6 @@ export class ContactsListComponent {
   }
 
   private searchList(term: string) {
-    return this.contacts$ = this.contactsService.search(term);
+    return this.contactsService.search(term);
   }
 }
