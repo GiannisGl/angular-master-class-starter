083f3729faedc8c2a212b3fcfc89fdd24c8eb29b
Ioannis Glampedakis <Ioannis.Glampedakis@netcetera.com>
2021-11-09 12:01:12 +0100

    observables: exercise 2

--- src/app/contacts-list/contacts-list.component.ts
+++ src/app/contacts-list/contacts-list.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {Contact} from '../models/contact';
 import {ContactsService} from '../contacts.service';
-import {Observable} from 'rxjs';
+import {Observable, Subject} from 'rxjs';
+import {debounceTime, distinctUntilChanged} from 'rxjs/operators';
 
 @Component({
   selector: 'trm-contacts-list',
@@ -10,15 +11,26 @@ import {Observable} from 'rxjs';
 })
 export class ContactsListComponent {
   contacts$: Observable<Contact[]> = this.contactsService.getContacts();
+  private terms$: Subject<string> = new Subject<string>();
 
   constructor(private contactsService: ContactsService) {
+    this.terms$
+      .pipe(
+        distinctUntilChanged(),
+        debounceTime(400)
+      )
+      .subscribe((term: string) => this.searchList(term));
   }
 
   trackById(index: number, contact: Contact): string | number {
     return contact.id;
   }
 
-  search(term: any): void {
-    this.contacts$ = this.contactsService.search(term);
+  search(term: string) {
+    this.terms$.next(term);
+  }
+
+  private searchList(term: string) {
+    return this.contacts$ = this.contactsService.search(term);
   }
 }
